64ed18a3f180cef70344b683b5587ec6638dff5b
Phil Renaud <phil@riotindustries.com>
2023-11-02 14:09:22 -0400

    First step for extensions

--- src/lib/note.ts
+++ src/lib/note.ts
@@ -28,6 +28,21 @@ export function notesForChord(chord: Chord): string[] {
 	const result = [];
 	const notes = notesForKey(chord.root + chord.quality);
 
+	// add extensions
+	let extensions = chord.extensions.split(/(?<=\d)(?=[♭♯])/).sort((a, b) => parseInt(a) - parseInt(b));
+	// for each extension, split up its accidental and number parts; flip around the accidental to be after the number for parsing
+	extensions.forEach((extension) => {
+		let accidental = extension.replace(/\d/, '');
+		let number = parseInt(extension.replace(/[♭♯]/, ''));
+		let noteIndex = (number - 1) % 7;
+		let note = notes[noteIndex];
+		if (accidental) {
+			note = note.replace(/♭|♯/, '');
+			note += accidental;
+		}
+		result.push(note);
+	});
+
 	result.push(notes[0]);
 	result.push(notes[2]);
 	result.push(notes[4]);
