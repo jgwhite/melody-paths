507a4bf6d720d1de625130127d2fa3d8024c4437
Jamie White <jamie@jgwhite.co.uk>
2023-12-28 15:35:30 +0100

    Fix Autumn Leaves issues

--- src/lib/songs/autumn-leaves.ts
+++ src/lib/songs/autumn-leaves.ts
@@ -10,6 +10,7 @@ export default {
 		{ bar: 5, root: 'Sol♯', quality: '-', extensions: '7♭5' },
 		{ bar: 6, root: 'Do♯', quality: '', extensions: '7' },
 		{ bar: 7, root: 'Fa♯', quality: '-', extensions: '' },
+		{ bar: 8, root: 'Fa♯', quality: '-', extensions: '' },
 
 		{ bar: 9, root: 'Si', quality: '-', extensions: '7' },
 		{ bar: 10, root: 'Mi', quality: '', extensions: '7' },
@@ -18,22 +19,26 @@ export default {
 		{ bar: 13, root: 'Sol♯', quality: '-', extensions: '7♭5' },
 		{ bar: 14, root: 'Do♯', quality: '', extensions: '7' },
 		{ bar: 15, root: 'Fa♯', quality: '-', extensions: '' },
+		{ bar: 16, root: 'Fa♯', quality: '-', extensions: '' },
 
-		{ bar: 16, root: 'Sol♯', quality: '-', extensions: '7♭5' },
-		{ bar: 17, root: 'Do♯', quality: '', extensions: '7♭9' },
-		{ bar: 18, root: 'Fa♯', quality: '-', extensions: '' },
-		{ bar: 20, root: 'Si', quality: '-', extensions: '7' },
-		{ bar: 21, root: 'Mi', quality: '', extensions: '7' },
-		{ bar: 22, root: 'La', quality: '', extensions: 'Δ' },
+		{ bar: 17, root: 'Sol♯', quality: '-', extensions: '7♭5' },
+		{ bar: 18, root: 'Do♯', quality: '', extensions: '7♭9' },
+		{ bar: 19, root: 'Fa♯', quality: '-', extensions: '' },
+		{ bar: 20, root: 'Fa♯', quality: '-', extensions: '' },
+		{ bar: 21, root: 'Si', quality: '-', extensions: '7' },
+		{ bar: 22, root: 'Mi', quality: '', extensions: '7' },
+		{ bar: 23, root: 'La', quality: '', extensions: 'Δ' },
+		{ bar: 24, root: 'La', quality: '', extensions: 'Δ' },
 
-		{ bar: 24, root: 'Sol♯', quality: '-', extensions: '7♭5' },
-		{ bar: 25, root: 'Do♯', quality: '', extensions: '7♭9' },
-		{ bar: 26, root: 'Fa♯', quality: '-', extensions: '7' },
+		{ bar: 25, root: 'Sol♯', quality: '-', extensions: '7♭5' },
+		{ bar: 26, root: 'Do♯', quality: '', extensions: '7♭9' },
+		{ bar: 27, root: 'Fa♯', quality: '-', extensions: '7' },
 		{ root: 'Si', quality: '', extensions: '7' },
-		{ bar: 27, root: 'Mi', quality: '-', extensions: '7' },
+		{ bar: 28, root: 'Mi', quality: '-', extensions: '7' },
 		{ root: 'La', quality: '', extensions: '7' },
 		{ bar: 29, root: 'Sol♯', quality: '-', extensions: '7♭5' },
 		{ bar: 30, root: 'Do♯', quality: '', extensions: '7♭9' },
-		{ bar: 31, root: 'Fa♯', quality: '-', extensions: '' }
+		{ bar: 31, root: 'Fa♯', quality: '-', extensions: '' },
+		{ bar: 32, root: 'Fa♯', quality: '-', extensions: '' }
 	]
 };
