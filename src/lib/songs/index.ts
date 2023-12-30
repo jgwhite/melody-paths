29df3fa363167c3994e9d71c37f16d6faabecf08
Jamie White <jamie@jgwhite.co.uk>
2023-12-31 00:34:31 +0100

    Add Take The “A” Train

--- src/lib/songs/index.ts
+++ src/lib/songs/index.ts
@@ -2,6 +2,7 @@ import autumnLeaves from './autumn-leaves';
 import blackOrpheus from './black-orpheus';
 import satinDoll from './satin-doll';
 import stThomas from './st-thomas';
+import takeTheATrain from './take-the-a-train';
 import youDontKnow from './you-dont-know-what-love-is';
 
-export default [autumnLeaves, blackOrpheus, satinDoll, stThomas, youDontKnow];
+export default [autumnLeaves, blackOrpheus, satinDoll, stThomas, takeTheATrain, youDontKnow];
---
+++ src/lib/songs/take-the-a-train.ts
@@ -0,0 +1,62 @@
+export default {
+	id: 'take-the-a-train',
+	name: 'Take The “A” Train',
+	key: 'Re',
+	sections: [
+		{
+			name: 'A',
+			chords: [
+				{ bar: 1, root: 'Re', quality: '', extensions: '6' },
+				{ bar: 2, root: 'Re', quality: '', extensions: '6' },
+				{ bar: 3, root: 'Mi', quality: '', extensions: '7♭5' },
+				{ bar: 4, root: 'Mi', quality: '', extensions: '7♭5' },
+				{ bar: 5, root: 'Mi', quality: '-', extensions: '7' },
+				{ bar: 6, root: 'La', quality: '', extensions: '7' },
+				{ bar: 7, root: 'Re', quality: '', extensions: '6' },
+				{ bar: 8, root: 'Mi', quality: '-', extensions: '7' },
+				{ root: 'La', quality: '', extensions: '7' }
+			]
+		},
+		{
+			name: 'A',
+			chords: [
+				{ bar: 9, root: 'Re', quality: '', extensions: '6' },
+				{ bar: 10, root: 'Re', quality: '', extensions: '6' },
+				{ bar: 11, root: 'Mi', quality: '', extensions: '7♭5' },
+				{ bar: 12, root: 'Mi', quality: '', extensions: '7♭5' },
+				{ bar: 13, root: 'Mi', quality: '-', extensions: '7' },
+				{ bar: 14, root: 'La', quality: '', extensions: '7' },
+				{ bar: 15, root: 'Re', quality: '', extensions: '6' },
+				{ bar: 16, root: 'La', quality: '-', extensions: '7' },
+				{ root: 'Re', quality: '', extensions: '7' }
+			]
+		},
+		{
+			name: 'B',
+			chords: [
+				{ bar: 17, root: 'Sol', quality: '', extensions: 'Δ' },
+				{ bar: 18, root: 'Sol', quality: '', extensions: 'Δ' },
+				{ bar: 19, root: 'Sol', quality: '', extensions: 'Δ' },
+				{ bar: 20, root: 'Sol', quality: '', extensions: 'Δ' },
+				{ bar: 21, root: 'Mi', quality: '', extensions: '7' },
+				{ bar: 22, root: 'Mi', quality: '', extensions: '7' },
+				{ bar: 23, root: 'Mi', quality: '-', extensions: '7' },
+				{ bar: 24, root: 'La', quality: '', extensions: '7' },
+				{ root: 'La', quality: '', extensions: '7♭9' }
+			]
+		},
+		{
+			name: 'A',
+			chords: [
+				{ bar: 25, root: 'Re', quality: '', extensions: '6' },
+				{ bar: 26, root: 'Re', quality: '', extensions: '6' },
+				{ bar: 27, root: 'Mi', quality: '', extensions: '7♭5' },
+				{ bar: 28, root: 'Mi', quality: '', extensions: '7♭5' },
+				{ bar: 29, root: 'Mi', quality: '-', extensions: '7' },
+				{ bar: 30, root: 'La', quality: '', extensions: '7' },
+				{ bar: 31, root: 'Re', quality: '', extensions: '6' },
+				{ bar: 32, root: 'Re', quality: '', extensions: '6' }
+			]
+		}
+	]
+};
