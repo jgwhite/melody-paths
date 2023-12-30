889f8eb3f00f6f334bb3c028858bacbd38cab03f
Jamie White <jamie@jgwhite.co.uk>
2023-12-30 23:40:54 +0100

    Incorporate sections
    Closes #7

--- src/lib/MelodyPath.svelte
+++ src/lib/MelodyPath.svelte
@@ -25,23 +25,55 @@
 	{/each}
 </select>
 
-<div class="progression">
-	<Column>
-		{#each notes as note}
-			<Dot>{note}</Dot>
-		{/each}
-	</Column>
+<div class="container">
+	<h1>{song.name}</h1>
 
-	{#each song.chords as chord (chord)}
-		<Chord key={song.key} {chord} />
+	{#each song.sections as section (section)}
+		<h2>{section.name}</h2>
+		<div class="progression">
+			<Column>
+				{#each notes as note}
+					<Dot>{note}</Dot>
+				{/each}
+			</Column>
+
+			{#each section.chords as chord (chord)}
+				<Chord key={song.key} {chord} />
+			{/each}
+		</div>
 	{/each}
 </div>
 
 <style>
+	.container {
+		display: inline-flex;
+		flex-direction: column;
+		position: relative;
+		gap: 8px;
+		padding: 1rem;
+	}
 	select {
 		margin-bottom: 1rem;
 	}
 	.progression {
 		display: flex;
+	}
+	h1 {
+		font-size: 1.25rem;
+	}
+	h2 {
+		display: flex;
+		font-size: 0.75rem;
+		position: relative;
+		gap: 8px;
+		align-items: center;
+		color: #555;
+	}
+	h2:after {
+		content: '';
+		display: block;
+		flex-grow: 1;
+		height: 1px;
+		background: #eee;
 	}
 </style>
