f900d5fddbb0a1b1e1b4b94c328488db09c1e2d4
Jamie White <jamie@jgwhite.co.uk>
2024-11-06 23:03:07 +0100

    Get started on my-one-and-only-love

--- src/lib/songs/index.ts
+++ src/lib/songs/index.ts
@@ -2,6 +2,7 @@ import allOfMe from './all-of-me';
 import allTheThingsYouAre from './all-the-things-you-are';
 import autumnLeaves from './autumn-leaves';
 import blackOrpheus from './black-orpheus';
+import myOneAndOnlyLove from './my-one-and-only-love';
 import satinDoll from './satin-doll';
 import stThomas from './st-thomas';
 import takeTheATrain from './take-the-a-train';
@@ -25,6 +26,7 @@ export default [
 	allTheThingsYouAre,
 	autumnLeaves,
 	blackOrpheus,
+	myOneAndOnlyLove,
 	satinDoll,
 	stThomas,
 	takeTheATrain,
---
+++ src/lib/songs/my-one-and-only-love.ts
@@ -0,0 +1,17 @@
+export default {
+	id: 'my-one-and-only-love',
+	name: 'My One and Only Love',
+	key: 'Re',
+	sections: [
+		{
+			name: 'A',
+			chords: [
+				{ bar: 1, root: 'Re', quality: '', extensions: 'Δ' },
+				{ root: 'Si', quality: '-', extensions: '7' },
+				{ bar: 2, root: 'Mi', quality: '-', extensions: '7' },
+				{ root: 'La', quality: '', extensions: '7' },
+				{ root: 'Si♭', quality: 'o', extensions: '7' }
+			]
+		}
+	]
+};
